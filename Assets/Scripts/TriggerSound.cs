552c24d6eace9a656f14d7de8a9550fe303c31be
OniHueAlloz <marinhoj07@gmail.com>
2024-10-30 15:39:05 -0300

    debug

---
+++ Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class TriggerSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip sound;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.loop = false;
+    }
+
+    // Update is called once per frame
+    /* void Update()
+    {
+        
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.PlayOneShot(sound);
+        }
+    }
+}
