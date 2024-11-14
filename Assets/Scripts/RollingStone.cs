8889886eeb886392bce324d650ac4e7e6ca0aecf
OniHueAlloz <marinhoj07@gmail.com>
2024-11-14 16:48:36 -0300

    RollingStone working

--- Assets/Scripts/RollingStone.cs
+++ Assets/Scripts/RollingStone.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 
 public class RollingStone : MonoBehaviour
 {
     public AudioClip rollSound;
     private AudioSource audioSource;
+    private Animator animator;
+    private Rigidbody rb;
+
+    [SerializeField] private float velocityThreshold = 0.1f;
+    private bool isMoving = false;
     
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
         audioSource.playOnAwake = false;
         audioSource.spatialBlend = 1.0f;
         audioSource.loop = false;
@@ -21,14 +30,32 @@ public class RollingStone : MonoBehaviour
     }
 
     // Update is called once per frame
-    /*void Update()
+    void Update()
     {
-        
-    } */
+        isMoving = MovementCheck();
+        //animator.SetBool("isMoving", isMoving);
+
+        if (isMoving && !audioSource.isPlaying)
+        {
+            PlayRollSound();
+        }
+    }
+
+    private bool MovementCheck()
+    {
+        float currentVelocity = rb.velocity.magnitude;
+
+        Debug.Log("Current Velocity: " + currentVelocity);
+
+        return currentVelocity > velocityThreshold;
+    }
 
     public void PlayRollSound()
     {
-        audioSource.PlayOneShot(rollSound);
-        Debug.Log("Roll sound played"); 
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(rollSound);
+            Debug.Log("Roll sound played");
+        }
     }
 }
